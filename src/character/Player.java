4182663ecdc4fa7f8ded8515410ff9f213c6ce9f
Ckrestene-Catapang <celeriocatapang12@gmail.com>
2025-11-25 22:33:48 +0800

    Update Player.java

--- src/character/Player.java
+++ src/character/Player.java
@@ -2,71 +2,98 @@ package character;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import game.Item;
 
 public class Player {
-    public enum ClassType { KNIGHT, ARCHER, MAGE }
-
     public String name;
     public ClassType classType;
-    public int hp, maxHp;
-    public int attack, defense;
-    public int mana, maxMana;
-    public int level;
-    public int exp;
-    public int floor;
+    public int hp, maxHp, mana, maxMana, attack, defense, level, floor;
     public List<Item> inventory = new ArrayList<>();
 
-    public Player(String name, ClassType classType) {
+    public enum ClassType { KNIGHT, ARCHER, MAGE }
+
+    // Constructor
+    public Player(String name, ClassType cls){
         this.name = name;
-        this.classType = classType;
+        this.classType = cls;
         this.level = 1;
-        this.exp = 0;
         this.floor = 1;
-        switch (classType) {
-            case KNIGHT: maxHp=40; hp=maxHp; attack=6; defense=4; mana=maxMana=0; break;
-            case ARCHER: maxHp=30; hp=maxHp; attack=7; defense=2; mana=maxMana=0; break;
-            case MAGE: maxHp=25; hp=maxHp; attack=4; defense=1; mana=maxMana=20; break;
+        this.maxHp = 30;
+        this.hp = maxHp;
+        this.attack = 5;
+        this.defense = 2;
+        if(cls == ClassType.MAGE){
+            this.maxMana = 20;
+            this.mana = maxMana;
         }
     }
 
-    public boolean isAlive() { return hp>0; }
-
-    public void gainExp(int amount) {
-        exp += amount;
-        int threshold = level*20;
-        while(exp>=threshold) { exp-=threshold; levelUp(); threshold=level*20; }
+    public void addItem(Item item){
+        inventory.add(item);
+        System.out.println(item.name + " added to inventory.");
     }
-
-    public void levelUp() {
-        level++;
-        switch(classType){
-            case KNIGHT: maxHp+=10; attack+=2; defense+=2; break;
-            case ARCHER: maxHp+=8; attack+=3; defense+=1; break;
-            case MAGE: maxHp+=6; attack+=1; defense+=1; maxMana+=5; mana=maxMana; break;
-        }
-        hp=maxHp;
-        System.out.println("-- Level up! You are now level "+level+" --");
-    }
-
-    public void addItem(Item item){ if(item!=null) inventory.add(item); }
 
     public void showInventory(){
-        if(inventory.isEmpty()){ System.out.println("Your inventory is empty."); return; }
-        System.out.println("-- Inventory --");
+        if(inventory.isEmpty()){
+            System.out.println("Your inventory is empty.");
+            return;
+        }
+        System.out.println("\n-- Inventory --");
         for(int i=0;i<inventory.size();i++){
-            Item it=inventory.get(i);
-            System.out.println(i+") "+it.name+" ["+it.type+"] (val: "+it.value+")");
+            Item item = inventory.get(i);
+            System.out.println(i + ") " + item.name + " [" + item.type + "] Value: " + item.value);
         }
     }
 
-    public void useItem(int index){
-        if(index<0||index>=inventory.size()){ System.out.println("Invalid item index."); return; }
-        Item item = inventory.get(index);
-        switch(item.type){
-            case POTION: System.out.println("You use "+item.name+" and heal "+item.value+" HP."); hp=Math.min(maxHp,hp+item.value); inventory.remove(index); break;
-            case WEAPON: System.out.println("You equip "+item.name+". Attack +"+item.value); attack+=item.value; inventory.remove(index); break;
-            case ARMOR: System.out.println("You equip "+item.name+". Defense +"+item.value); defense+=item.value; inventory.remove(index); break;
+    public void useItem(int idx){
+        if(idx < 0 || idx >= inventory.size()){
+            System.out.println("Invalid item index.");
+            return;
         }
+        Item item = inventory.get(idx);
+        if(item.type == Item.ItemType.POTION){
+            hp = Math.min(maxHp, hp + item.value);
+            System.out.println("Used " + item.name + " and recovered " + item.value + " HP.");
+        } else {
+            System.out.println("You can't use " + item.name + " right now!");
+        }
+        inventory.remove(idx);
+    }
+
+    public boolean isAlive(){ return hp > 0; }
+
+    public void gainExp(int exp){
+        System.out.println("Gained " + exp + " EXP.");
+        level++;
+        maxHp += 5;
+        hp = maxHp;
+        attack += 1;
+        defense += 1;
+        if(classType == ClassType.MAGE){
+            maxMana += 3;
+            mana = maxMana;
+        }
+        System.out.println("Leveled up! Now Lv" + level);
+    }
+
+    public void showSkills() {
+        System.out.println("\n-- Skills --");
+        System.out.println("1) Basic Attack");
+        switch(this.classType) {
+            case KNIGHT:
+                System.out.println("2) Shield Bash");
+                System.out.println("3) Power Strike");
+                break;
+            case ARCHER:
+                System.out.println("2) Quick Shot");
+                System.out.println("3) Rain of Arrows");
+                break;
+            case MAGE:
+                System.out.println("2) Ice Shard");
+                System.out.println("3) Fireball");
+                break;
+        }
+        System.out.print("> ");
     }
 }
